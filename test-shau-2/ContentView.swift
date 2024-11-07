fe0a8e1e5d66062b4af74474273d6cfb761aaec4
shaunak <su82@cornell.edu>
2024-11-07 10:57:02 -0500

    Initial Commit

---
+++ test-shau-2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test-shau-2
+//
+//  Created by Shaunak Umarkhedi on 07/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test-shau-2/test_shau_2App.swift
@@ -0,0 +1,17 @@
+//
+//  test_shau_2App.swift
+//  test-shau-2
+//
+//  Created by Shaunak Umarkhedi on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct test_shau_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
